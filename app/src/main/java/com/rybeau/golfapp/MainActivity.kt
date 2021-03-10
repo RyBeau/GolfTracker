7c3443b19b919aa75c8477cf838c5973d6e67819
Ryan Beaumont <ryan.beaumont1@gmail.com>
2021-03-10 19:40:11 +1300

    add strings and create basic layout

--- app/src/main/java/com/rybeau/golfapp/MainActivity.kt
+++ app/src/main/java/com/rybeau/golfapp/MainActivity.kt
@@ -6,6 +6,11 @@ import android.os.Bundle
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        try {
+            this.getSupportActionBar()?.hide()
+        } catch (error : NullPointerException) {}
+
         setContentView(R.layout.activity_main)
     }
 }
