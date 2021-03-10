58f4fa6a9c7a2650d25d010c8fb50f6f524f25a8
Ryan Beaumont <ryan.beaumont1@gmail.com>
2021-03-10 21:14:38 +1300

    Update method of removing titlebar

--- app/src/main/java/com/rybeau/golfapp/MainActivity.kt
+++ app/src/main/java/com/rybeau/golfapp/MainActivity.kt
@@ -8,10 +8,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        try {
-            this.getSupportActionBar()?.hide()
-        } catch (error : NullPointerException) {}
-
         setContentView(R.layout.activity_main)
     }
 }
