30204841081daa99ac45e30a38659f6c5a5fae80
Ryan Beaumont <ryan.beaumont1@gmail.com>
2021-03-11 20:54:03 +1300

    Initialise PreviousRoundsFragment.kt

---
+++ app/src/main/java/com/rybeau/golfapp/PreviousRoundsFragment.kt
@@ -0,0 +1,18 @@
+package com.rybeau.golfapp
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class PreviousRoundsFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_previous_rounds, container, false)
+    }
+}
