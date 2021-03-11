e0cc72c4ca0c86af9369e4c9cee256cf0c9ab246
Ryan Beaumont <ryan.beaumont1@gmail.com>
2021-03-11 22:04:25 +1300

    Create previous rounds with data

--- app/src/main/java/com/rybeau/golfapp/Round.kt
+++ app/src/main/java/com/rybeau/golfapp/Round.kt
@@ -1,6 +1,8 @@
 package com.rybeau.golfapp
 
+import java.time.LocalDate
+import java.time.LocalDateTime
 import java.util.*
 
-class Round (val date: Date, val score: String, val averagePutts: Int) {
+class Round (val date: String, val score: String, val averagePutts: Int) {
 }
