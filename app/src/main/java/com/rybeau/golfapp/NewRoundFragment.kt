4d6a913ed8da2b0981cc45732fed6b680ecb06c7
Ryan Beaumont <ryan.beaumont1@gmail.com>
2021-03-11 16:34:38 +1300

    More layout adjustments

--- app/src/main/java/com/rybeau/golfapp/NewRoundFragment.kt
+++ app/src/main/java/com/rybeau/golfapp/NewRoundFragment.kt
@@ -10,6 +10,7 @@ import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.chip.Chip
 
 class NewRoundFragment : Fragment() {
 
@@ -26,7 +27,25 @@ class NewRoundFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        val nineButton = view.findViewById<Chip>(R.id.nineHoles)
+        val eighteenButton = view.findViewById<Chip>(R.id.eighteenHoles)
+
+        nineButton.setOnClickListener{
+            updateHoles(9)
+        }
+
+        eighteenButton.setOnClickListener{
+            updateHoles(18)
+        }
+
         createInput(view)
+    }
+
+    fun updateHoles(holes: Int) {
+        if (holes != numHoles){
+            numHoles = holes
+            this.view?.let { createInput(it) }
+        }
     }
 
     private fun createInput(view: View){
