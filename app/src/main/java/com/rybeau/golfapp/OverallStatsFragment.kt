1f682f64a6f774e8971a0bc5a2f8f94a2e36cc1b
Ryan Beaumont <ryan.beaumont1@gmail.com>
2021-03-12 16:03:52 +1300

    placeholder stat population

--- app/src/main/java/com/rybeau/golfapp/OverallStatsFragment.kt
+++ app/src/main/java/com/rybeau/golfapp/OverallStatsFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 
 class OverallStatsFragment : Fragment() {
 
@@ -12,6 +13,24 @@ class OverallStatsFragment : Fragment() {
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_overall_stats, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        populateStats(view)
+    }
+
+    private fun populateStats(view: View) {
+        val averageScore = view.findViewById<TextView>(R.id.averageScore)
+        val handicap = view.findViewById<TextView>(R.id.handicap)
+        val puttsPerHole = view.findViewById<TextView>(R.id.puttsPerHole)
+        val totalRounds = view.findViewById<TextView>(R.id.totalRounds)
+
+        averageScore.text = "+13"
+        handicap.text = "13"
+        puttsPerHole.text = "3"
+        totalRounds.text = "115"
     }
 
 }
