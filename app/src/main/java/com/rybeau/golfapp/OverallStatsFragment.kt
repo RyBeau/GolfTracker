258810d514d1b0448757c68861a6b19f6f81dc43
Ryan Beaumont <ryan.beaumont1@gmail.com>
2021-03-12 17:46:21 +1300

    Import change

--- app/src/main/java/com/rybeau/golfapp/OverallStatsFragment.kt
+++ app/src/main/java/com/rybeau/golfapp/OverallStatsFragment.kt
@@ -8,10 +8,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
-import com.github.aachartmodel.aainfographics.aachartcreator.AAChartModel
-import com.github.aachartmodel.aainfographics.aachartcreator.AAChartType
-import com.github.aachartmodel.aainfographics.aachartcreator.AAChartView
-import com.github.aachartmodel.aainfographics.aachartcreator.AASeriesElement
+import com.github.aachartmodel.aainfographics.aachartcreator.*
 
 class OverallStatsFragment : Fragment() {
 
