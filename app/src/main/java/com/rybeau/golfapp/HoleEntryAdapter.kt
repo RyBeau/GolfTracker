828923b4fd84a364069b5a0dbae51d1df1aae2d8
Ryan Beaumont <ryan.beaumont1@gmail.com>
2021-03-23 21:13:38 +1300

    Coverted to use with list view

--- app/src/main/java/com/rybeau/golfapp/HoleEntryAdapter.kt
+++ app/src/main/java/com/rybeau/golfapp/HoleEntryAdapter.kt
@@ -1,30 +1,52 @@
 package com.rybeau.golfapp
 
-import android.text.InputFilter
+import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.BaseAdapter
 import android.widget.EditText
 import android.widget.TextView
-import androidx.recyclerview.widget.RecyclerView
 
-class HoleEntryAdapter(private val holes: IntArray)
-    : RecyclerView.Adapter<HoleEntryAdapter.HoleEntryViewHolder>()
-{
-    class HoleEntryViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+class HoleEntryAdapter(context: Context, private val holes: IntArray) : BaseAdapter() {
+
+    private val inflater: LayoutInflater = LayoutInflater.from(context)
+
+    class HoleEntryViewHolder(itemView: View) {
         val holeNumber: TextView = itemView.findViewById<TextView>(R.id.holeNumber)
         val parInput: EditText = itemView.findViewById<EditText>(R.id.parInput)
         val scoreInput: EditText = itemView.findViewById<EditText>(R.id.scoreInput)
         val puttsInput: EditText = itemView.findViewById<EditText>(R.id.puttsInput)
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): HoleEntryViewHolder {
-        val view = LayoutInflater.from(parent.context)
-                .inflate(R.layout.hole_entry_item, parent.findViewById(R.id.inputView), false)
-        return HoleEntryViewHolder(view)
+    override fun getCount(): Int {
+        return holes.size
     }
 
-    override fun onBindViewHolder(viewHolder: HoleEntryViewHolder, position: Int) {
+    override fun getItem(position: Int): Any {
+        return holes[position]
+    }
+
+    override fun getItemId(position: Int): Long {
+        return position.toLong()
+    }
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View? {
+        val view: View?
+        val viewHolder: HoleEntryViewHolder
+        if (convertView == null) {
+            view = inflater.inflate(R.layout.hole_entry_item, parent, false)
+            viewHolder = HoleEntryViewHolder(view)
+            view.tag = viewHolder
+        } else {
+            view = convertView
+            viewHolder = view.tag as HoleEntryViewHolder
+        }
+        fillViewHolder(viewHolder, position)
+        return view
+    }
+
+    private fun fillViewHolder(viewHolder: HoleEntryViewHolder, position: Int) {
         viewHolder.holeNumber.text = holes[position].toString()
         viewHolder.parInput.tag = "par${holes[position]}"
         viewHolder.parInput.filters = arrayOf(HoleEntryFilter(1, 5))
@@ -33,6 +55,5 @@ class HoleEntryAdapter(private val holes: IntArray)
         viewHolder.puttsInput.tag = "putts${holes[position]}"
         viewHolder.puttsInput.filters = arrayOf(HoleEntryFilter(1, 20))
     }
-
-    override fun getItemCount() = holes.size
 }
+
