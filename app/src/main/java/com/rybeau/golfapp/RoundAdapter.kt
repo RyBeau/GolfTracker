4e4c8cd97862a9e50bb1cf9b8a3b6cc36f00ee6d
Ryan Beaumont <ryan.beaumont1@gmail.com>
2021-03-25 22:55:35 +1300

    Fix rounds not showing up bug

--- app/src/main/java/com/rybeau/golfapp/RoundAdapter.kt
+++ app/src/main/java/com/rybeau/golfapp/RoundAdapter.kt
@@ -45,5 +45,6 @@ class RoundAdapter(private var rounds: List<Round>, private val onRoundListener:
 
     fun setData(newRounds: List<Round>){
         rounds = newRounds
+        notifyDataSetChanged()
     }
 }
