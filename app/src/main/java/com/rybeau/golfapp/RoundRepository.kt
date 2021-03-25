9b093919e88ccc48446649f319903d4340043912
Ryan Beaumont <ryan.beaumont1@gmail.com>
2021-03-25 21:48:04 +1300

    Add missing type def

--- app/src/main/java/com/rybeau/golfapp/RoundRepository.kt
+++ app/src/main/java/com/rybeau/golfapp/RoundRepository.kt
@@ -6,7 +6,7 @@ import kotlinx.coroutines.flow.Flow
 class RoundRepository(private val roundDao: RoundDao) {
     val allRounds: Flow<List<Round>> = roundDao.getAll()
     val totalRounds: Flow<Int> = roundDao.getTotalRounds()
-    val previous10Rounds = roundDao.getPrevious10()
+    val previous10Rounds: Flow<List<Round>> = roundDao.getPrevious10()
 
     @WorkerThread
     suspend fun insert(round: Round){
