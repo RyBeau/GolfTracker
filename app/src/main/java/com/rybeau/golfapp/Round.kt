cf5c10f3c6b0aefa8f69f15d3343f65b25e49fae
Ryan Beaumont <ryan.beaumont1@gmail.com>
2021-03-25 21:02:56 +1300

    Update for room

--- app/src/main/java/com/rybeau/golfapp/Round.kt
+++ app/src/main/java/com/rybeau/golfapp/Round.kt
@@ -1,8 +1,23 @@
 package com.rybeau.golfapp
 
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import java.time.LocalDate
 import java.time.LocalDateTime
 import java.util.*
 
-class Round (val date: String, val score: Int, val averagePutts: Double) {
+@Entity(tableName = "round")
+class Round (
+    @ColumnInfo var date: String,
+    @ColumnInfo var score: Int,
+    @ColumnInfo var averagePutts: Double) {
+
+    @PrimaryKey(autoGenerate = true) var id: Long = 0
+
+    override fun toString(): String {
+        return "Date: $date" +
+                "Score: $score" +
+                "Average Putts: ${String.format("%.1f", averagePutts)}"
+    }
 }
