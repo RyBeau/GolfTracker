cd2c1386f4f3d9ab117c5c303552b1fb893d59fa
Ryan Beaumont <ryan.beaumont1@gmail.com>
2021-03-26 00:11:34 +1300

    Update for type change and new queries

--- app/src/main/java/com/rybeau/golfapp/RoundRepository.kt
+++ app/src/main/java/com/rybeau/golfapp/RoundRepository.kt
@@ -6,7 +6,9 @@ import kotlinx.coroutines.flow.Flow
 class RoundRepository(private val roundDao: RoundDao) {
     val allRounds: Flow<List<Round>> = roundDao.getAll()
     val totalRounds: Flow<Int> = roundDao.getTotalRounds()
-    val previous10Rounds: Flow<List<Round>> = roundDao.getPrevious10()
+    val previous10RoundsScore: Flow<List<Int>> = roundDao.getPrevious10Score()
+    val averageScore: Flow<Double> = roundDao.getAverageScore()
+    val averagePutts: Flow<Double> = roundDao.getAveragePutts()
 
     @WorkerThread
     suspend fun insert(round: Round){
