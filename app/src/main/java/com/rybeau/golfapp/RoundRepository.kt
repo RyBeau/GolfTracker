55f85a853fc1cc82a422f66e8800eb53b1835ed1
Ryan Beaumont <ryan.beaumont1@gmail.com>
2021-03-25 21:27:26 +1300

    Create the repository

---
+++ app/src/main/java/com/rybeau/golfapp/RoundRepository.kt
@@ -0,0 +1,20 @@
+package com.rybeau.golfapp
+
+import androidx.annotation.WorkerThread
+import kotlinx.coroutines.flow.Flow
+
+class RoundRepository(private val roundDao: RoundDao) {
+    val allRounds: Flow<List<Round>> = roundDao.getAll()
+    val totalRounds: Flow<Int> = roundDao.getTotalRounds()
+    val previous10Rounds = roundDao.getPrevious10()
+
+    @WorkerThread
+    suspend fun insert(round: Round){
+        roundDao.insert(round)
+    }
+
+    @WorkerThread
+    suspend fun delete(round: Round){
+        roundDao.delete(round)
+    }
+}
